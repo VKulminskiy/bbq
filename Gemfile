ca6fee13ef0783730320fc7a495d4add864188cd
wkylminskii <w_kylminskii@mail.ru>
2020-08-11 15:44:51 +0300

    first alpha version

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'bootsnap'
 
 gem 'listen', '~> 3.2'
 
+gem 'devise'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,9 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
+
+  before_action :set_event, only: [:show]
+  # Здесь уже перечисляем все действия, доступные конкретному юзеру
+  before_action :set_current_user_event, only: [:edit, :update, :destroy]
 
   # GET /events
   def index
@@ -12,7 +16,7 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
-    @event = Event.new
+    @event = current_user.events.build
   end
 
   # GET /events/1/edit
@@ -21,7 +25,7 @@ class EventsController < ApplicationController
 
   # POST /events
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.build(event_params)
 
     if @event.save
       redirect_to @event, notice: 'Event was successfully created.'
@@ -46,6 +50,13 @@ class EventsController < ApplicationController
   end
 
   private
+
+    # Будем искать событие не среди всех,
+    # а только у текущего пользователя по id
+    def set_current_user_event
+      @event = current_user.events.find(params[:id])
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
